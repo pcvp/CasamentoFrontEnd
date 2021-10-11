e6c4c99e252ef23d938189f81b17776f43bd0844
Paulo Vieira <45016641+pcvp@users.noreply.github.com>
2021-10-11 08:33:28 -0300

    Title do site

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,25 @@
 // add bootstrap css
-import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.css";
-import "../styles/globals.css";
-import "../styles/overides.css";
-import Layout from "./layout";
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
+import '../styles/overides.css'
+import Layout from './layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
         <>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/libs/fontawesome/css/all.min.css" rel="stylesheet" />
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <title>Paulo e Fabiana</title>
+          <link href='/libs/fontawesome/css/all.min.css' rel='stylesheet' />
         </>
       </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
